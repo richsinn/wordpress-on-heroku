bf64697ef6de80a37de8dbe1d3cd926a0d643478
Richard Sinn <richsinn@gmail.com>
2013-03-08 00:06:46 -0800

    minor changes header.php

--- config/public/wp-content/themes/toolbox/header.php
+++ config/public/wp-content/themes/toolbox/header.php
@@ -64,14 +64,15 @@
 			<h1 id="site-title"><a href="<?php //echo home_url( '/' ); ?>" title="<?php //echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 id="site-description"><?php //bloginfo( 'description' ); ?></h2>
 		</hgroup>
-
+    -->
+    <!--
 		<nav id="access" role="navigation">
 			<h1 class="assistive-text section-heading"><//?php _e( 'Main menu', 'toolbox' ); ?></h1>
 			<div class="skip-link screen-reader-text"><a href="#content" title="<?//php esc_attr_e( 'Skip to content', 'toolbox' ); ?>"><?php _e( 'Skip to content', 'toolbox' ); ?></a></div>
-
+    
 			<?//php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-    </nav><!-- #access -->
-    -->
+    </nav>--><!-- #access -->
+    
     <div class="navbar-inner">
       <div class="container">
         <div class="row">
